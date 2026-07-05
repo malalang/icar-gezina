9d09783cfab97d9931947fad0b6cf876c29c633a
Malalang <malalangptyltd@gmail.com>
2026-07-05 10:51:08 +0200

    Add new hostname to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**', // This allows any path under the hostname
       },
+        {
+        protocol: 'https',
+        hostname: 'icargezina.co.za',
+        port: '',
+        pathname: '/**', // This allows any path under the hostname
+      }
     ],
   },
   output: 'standalone',
